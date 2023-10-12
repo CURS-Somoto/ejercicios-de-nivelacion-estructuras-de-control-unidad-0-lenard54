9ef9ad266277403fbf469b5cd52ad52efd289e71
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 16:20:30 -0600

    Update Ejercicio_8.cs
    Este es el ejercicio 8

--- Ejercicio_8.cs
+++ Ejercicio_8.cs
@@ -20,3 +20,32 @@ la siguiente secuencia de patrones:
 *****
 
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impresion_de_patrones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+                Console.WriteLine();  
+               
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
