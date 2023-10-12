99bcb425881b1db1febbc6eb68b56d256b3217fa
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 16:16:14 -0600

    Update Ejercicio_7.cs
    Este es el ejercicio 7

--- Ejercicio_7.cs
+++ Ejercicio_7.cs
@@ -4,3 +4,24 @@
 Imprime un conteo regresivo del 10 al 1
 utilizando un bucle for.
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuenta_regresiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int cuenta = 10; cuenta > 0; cuenta--)
+            {
+                Console.WriteLine(cuenta);
+            }
+            Console.ReadKey();
+        }
+    }
+}
