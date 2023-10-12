fb01c5f3d29fa9e99316d7a73342f0d10f98e801
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 16:26:26 -0600

    Update Ejercicio_9.cs
    Este es el ejercicio 9

--- Ejercicio_9.cs
+++ Ejercicio_9.cs
@@ -6,3 +6,37 @@ Dado dos números, muestra el número mayor
 utilizando una estructura if
 
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mayor_de_dos_numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el primer numero");
+            int numero1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo numero");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+
+            if (numero1 > numero2)
+            {
+                Console.WriteLine("El numero mayor es " +numero1);
+            }
+            else if (numero1 < numero2) 
+            {
+                Console.WriteLine("El numero mayor es " +numero2);
+            }
+            else
+            {
+                Console.WriteLine("Los valores de los numeros deben de ser diferentes");
+            }
+            Console.ReadLine();
+        }
+    }
+}
