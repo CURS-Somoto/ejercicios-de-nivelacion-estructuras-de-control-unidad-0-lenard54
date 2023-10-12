cdeb5f6c2c7d02c35c7d163ac3155527392712cd
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 15:59:39 -0600

    Update Ejercicio_5.cs
    Esta es la tabla de multiplicar que nos solicito en clases y me dijo que la sustituyera por el ejercicio repetido

--- Ejercicio_5.cs
+++ Ejercicio_5.cs
@@ -3,3 +3,31 @@
 
 Escribe un programa en C# que determine si
 un número entero dado es positivo, negativo o cero.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el numero Para de la tabla de Multiplicar");
+            int numero = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < 11; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine(numero + "*" + i + "=" + resultado);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
