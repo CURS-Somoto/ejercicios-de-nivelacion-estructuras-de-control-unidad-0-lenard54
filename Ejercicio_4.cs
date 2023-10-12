67e30f730d0395dc0c66c0611564d2ff1068ca11
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 15:51:45 -0600

    Update Ejercicio_4.cs
    Este es el ejercicio 4

--- Ejercicio_4.cs
+++ Ejercicio_4.cs
@@ -3,3 +3,34 @@
 
 Crea un programa que genere y muestre el
 resultado factorial de un numero. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_de_un_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba un numero");
+
+            int multiplicacion = 1;
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            while (numero > 0)
+            {
+                multiplicacion = multiplicacion * numero;
+                numero--;
+              //Console.WriteLine() lo escribi dentro del metodo para ver todos los resultados
+                Console.WriteLine(multiplicacion);
+            }
+           //Pero se que si lo escribo afuera solamente va a imprimir el ultimo resultado del bucle.
+            Console.ReadKey();
+        }
+    }
+}
+
