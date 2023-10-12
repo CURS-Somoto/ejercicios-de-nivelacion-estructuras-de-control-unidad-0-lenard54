f98775424c026e1b211bd6cab2c133e715675737
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 15:21:31 -0600

    Update Ejercicio_1.cs
    Este es el primer ejercicio

--- Ejercicio_1.cs
+++ Ejercicio_1.cs
@@ -3,3 +3,27 @@
 
 Escribe un programa
 que muestre los números pares del 2 al 20. */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impresión_de_numeros_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (i = 2; i < 21; i = i + 2)
+            {
+             Console.WriteLine(Convert.ToString(i));
+            }
+            Console.ReadKey();
+           
+
+        }
+    }
+}
+
