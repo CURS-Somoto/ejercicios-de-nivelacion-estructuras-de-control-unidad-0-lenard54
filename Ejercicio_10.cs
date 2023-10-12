f3f4c61d6c66376a33251543fcfdbb544c6deb42
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 16:29:41 -0600

    Update Ejercicio_10.cs
    Este es el ejercicio 10

--- Ejercicio_10.cs
+++ Ejercicio_10.cs
@@ -6,3 +6,35 @@ Positivo, Negativo o Cero
 Escribe un programa en C# que determine si
 un número entero dado es positivo, negativo o cero.
 */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Determinar_negatio_positivo_o_cero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdusca un numero");
+
+           int numero = Convert.ToInt32(Console.ReadLine());
+
+           if (numero > 0) 
+           {
+               Console.WriteLine(" Es positivo");
+           }
+           else if (numero == 0) {
+               Console.WriteLine("Es cero");
+           }
+           else{
+               Console.WriteLine("Es negativo");
+           }
+          
+           Console.ReadKey();
+           }
+      }
+}
