26749c0b5253ebf28f40bc35355824b221178f9d
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 15:25:32 -0600

    Update Ejercicio_2.cs
    Este es el segundo ejercicio

--- Ejercicio_2.cs
+++ Ejercicio_2.cs
@@ -3,3 +3,34 @@
 
 Crea un programa que calcule y muestre la
 suma de los dígitos de un número ingresado por el usuario.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Suma_de_digitos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            int suma = 0;
+
+            while (numero != 0) 
+            {
+                int digito = numero % 10;
+                suma += digito;
+                numero /= 10;
+            }
+
+            Console.WriteLine("La suma de los digitos es: " + suma);
+
+            Console.ReadLine();
+        }
+    }
+}
