7e99e721c6936308afe65d8a8880a508ece8d122
Binary_Life <147262828+lenard54@users.noreply.github.com>
2023-10-12 16:01:43 -0600

    Update Ejercicio_6.cs
    Este es el ejercicio 6

--- Ejercicio_6.cs
+++ Ejercicio_6.cs
@@ -3,3 +3,33 @@
 
 Escribe un programa que diga si un número
 es par o impar.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Par_e_impar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            else 
+            {
+                Console.WriteLine(" El numero es impar");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
+
